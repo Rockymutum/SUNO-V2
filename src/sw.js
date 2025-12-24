78d82be7803c2b291c787e3550ee84e1efb57deb
Rockymutum <rockymutum786@gmail.com>
2025-12-24 21:48:08 +0530

    CHanges

--- src/sw.js
+++ src/sw.js
@@ -43,7 +43,7 @@ self.addEventListener('push', (event) => {
 self.addEventListener('notificationclick', (event) => {
     event.notification.close()
 
-    const urlToOpen = event.notification.data?.url || '/'
+    const urlToOpen = new URL(event.notification.data?.url || '/', self.location.origin).href
 
     event.waitUntil(
         clients.matchAll({ type: 'window', includeUncontrolled: true }).then((windowClients) => {
